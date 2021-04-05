7506d5ecc11e173f654ff6a31d16b64561198ccf
KIRK <kirki@otc.edu>
2021-04-05 09:49:11 -0500

    Basic button plus code i dunno whats going on yet

--- app/src/main/java/com/example/testapis/MainActivity.java
+++ app/src/main/java/com/example/testapis/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.testapis;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -10,27 +12,36 @@ import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnGet;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-/*
-        String page = new Communicator().executeHttpGet("Some URL");
-        JSONArray jsonArray = null;
-        try {
-            jsonArray = new JSONArray(page);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        for (int i = 0 ; i < jsonArray.length(); i++ ) {
-            try {
-                JSONObject entry = (JSONObject) jsonArray.get(i);
-            } catch (JSONException e) {
-                e.printStackTrace();
+        btnGet.findViewById(R.id.btnGet);
+
+        btnGet.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                JSONArray jsonArray = null;
+                try {
+                    jsonArray = new JSONArray(page);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                for (int i = 0 ; i < jsonArray.length(); i++ ) {
+                    try {
+                        JSONObject entry = (JSONObject) jsonArray.get(i);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    // now get the data from each entry
+                }
             }
-            // now get the data from each entry
-        }
+        });
+/*
 
  */
 
